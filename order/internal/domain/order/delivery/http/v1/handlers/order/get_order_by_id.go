993fcec20619629f9cdde93ac1de65051dd60567
Артём Кулик <artem-kulik@list.ru>
2026-02-23 23:43:38 +0300

    check ci/cd

--- order/internal/domain/order/delivery/http/v1/handlers/order/get_order_by_id.go
+++ order/internal/domain/order/delivery/http/v1/handlers/order/get_order_by_id.go
@@ -30,7 +30,7 @@ func (h *OrderHandler) GetOrderByID(ctx *gin.Context) {
 		middleware.MapErrors(ctx, err) //TODO: middleware
 		return
 	}
-	data.PaymentMethod = "cash"
+	data.PaymentMethod = "asdasd"
 	ctx.JSON(http.StatusOK, common.Response{
 		Status: http.StatusOK,
 		Data:   data,
